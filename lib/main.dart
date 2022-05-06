c6f398cea043ce393722f8f47c6314fc1119faa3
Arian <arian_febrian_29rpl@student.smktelkom-mlg.sch.id>
2022-05-06 08:50:43 +0700

    Lesson/19

--- lib/main.dart
+++ lib/main.dart
@@ -14,10 +14,39 @@ class Quotelist extends StatefulWidget {
 class _QuotelistState extends State<Quotelist> {
 
   List<Quote> quotes = [
-    Quote(author: 'r3nyah', text: 'Be yourself; everyone else is already taken'),
-    Quote(author: 'r3nyah', text: 'I have nothing to declare except my genius'),
-    Quote(author: 'r3nyah', text: 'The truth is rarely pure and never simple')
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
   ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+        margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                quote.text,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey[600],
+                ),
+              ),
+              SizedBox(height: 6.0),
+              Text(
+                quote.author,
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,10 +54,10 @@ class _QuotelistState extends State<Quotelist> {
       appBar: AppBar(
         title: Text('Awesome Quote'),
         centerTitle: true,
-        backgroundColor: Colors.redAccent,
+        backgroundColor: Colors.grey[850],
       ),
       body: Column(
-        children: quotes.map((quote) => Text('${quote.text} - ${quote.author}')).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
